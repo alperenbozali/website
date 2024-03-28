22640fd7ee3d81870cab6e591475a16685bb2b3c
batuhankaansozen <kaansozen121@gmail.com>
2024-03-28 10:56:19 +0300

    sayfa-yönlendirme/header-değişiklik

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 const express = require('express');
-const app = express();
-const port = 3000;
-const path = require('path');
+const app = express();  
+const path  = require('path');
 
-app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, './assets/css/style.css')));
+app.set('view engine', 'ejs');
 
+//Views klasörünü tanımlıyoruz.
+app.set('views', path.join(__dirname, 'views'));
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+//Middleware - Statik dosyaları (örneğin css, js dosyaları) sunucuya açmak için kullanılır.
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Ana sayfa için route
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-    
-    });
+//Ürün sayfası için route
+app.get('/urunler', (req, res) => {
+    res.render('urunler');
+});
 
-    app.get('/alperen', (req, res) => {
-        res.sendFile(path.join(__dirname, 'index.html'));
-        
-        });
+//Ürün detay sayfası için route
+app.get('/urunler/:id', (req, res) => {
+    res.render('urun-detay');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server ${PORT} portunda çalışıyor.`);
+});
