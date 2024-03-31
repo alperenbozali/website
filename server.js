4245b43f3c7dfa2d98a1553f65d77af607dc9edc
batuhankaansozen <kaansozen121@gmail.com>
2024-03-31 14:50:55 +0300

    update:datajson

--- server.js
+++ server.js
@@ -1,47 +1,52 @@
-const { log } = require('console');
-const express = require('express');
-const app = express();  
-const path  = require('path');
+const { log } = require("console");
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+//JSON dosyasını okuyoruz.
+const data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
 
 //Views klasörünü tanımlıyoruz.
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 //Middleware - Statik dosyaları (örneğin css, js dosyaları) sunucuya açmak için kullanılır.
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Ana sayfa için route
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 //Ürün sayfası için route
-app.get('/urunler', (req, res) => {
-    res.render('urunler');
-    console.log('Ürünler sayfası açıldı.');
+app.get("/urunler", (req, res) => {
+  res.render("urunler");
+  console.log("Ürünler sayfası açıldı.");
 });
 
-app.get('/urunler/:id', (req, res) => {
-    console.log('Ürün detay sayfası açıldı.');
-    console.log(req.params.id);
-    res.render('urun-detay', {id: req.params.id});
+app.get("/urunler/:id", (req, res) => {
+  const productId = parseInt(req.params.id);
+  const product = data.products.find((product) => product.id === productId);
+  if(!product){
+    res.status(404).send("Ürün bulunamadı.");
+    return;
+  }
+  res.render("urun-detay", { product });
 });
 
-app.get('/hakkimizda', (req, res) => {
-    console.log('Hakkımızda sayfası açıldı.');
-    res.render('about');
+app.get("/hakkimizda", (req, res) => {
+  console.log("Hakkımızda sayfası açıldı.");
+  res.render("about");
 });
 
-app.get('/iletisim', (req, res) => {
-    console.log('İletisim sayfası açıldı.');
-    res.render('contact');
+app.get("/iletisim", (req, res) => {
+  console.log("İletisim sayfası açıldı.");
+  res.render("contact");
 });
-
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server ${PORT} portunda çalışıyor.`);
+  console.log(`Server ${PORT} portunda çalışıyor.`);
 });
